2323f5f0156b7304b8a47304308da92592f2845d
rym12 <ryuma@DESKTOP-LTG4VBB>
2023-10-03 00:18:30 +0900

    バグってたので訂正しました

--- resources/views/tweet/comment.blade.php
+++ resources/views/tweet/comment.blade.php
@@ -1,42 +1,45 @@
 <x-app-layout>
-  <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight dark:text-gray-200">
-      {{ __('Show Comment') }}
-    </h2>
-  </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight dark:text-gray-200">
+            {{ __('Comments') }}
+        </h2>
+    </x-slot>
 
-  <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:w-8/12 md:w-1/2 lg:w-5/12">
-      <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-800">
-          <div class="mb-6">
-            @if($comments)
-              @foreach($comments as $comment)
-                  <div class="flex flex-col mb-4">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:w-8/12 md:w-1/2 lg:w-5/12">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-800">
+                    <div class="mb-6">
                       <p class="mb-2 uppercase font-bold text-lg text-gray-800 dark:text-gray-200">Comment</p>
-                      <p class="py-2 px-3 text-gray-800 dark:text-gray-200" id="comment">  
-                          {{ $comment->content }}
-                      </p>
-                  </div>
-              @endforeach
-            @endif
-            <div class="flex items-center justify-end mt-4">
-              <a href="{{ url()->previous() }}">
-                <x-secondary-button class="ml-3">
-                  {{ __('Back') }}
-                </x-secondary-button>
-              </a>
-              <a href="{{ route('tweet.send',$tweet->id) }}">
-                <x-secondary-button class="ml-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z" />
-                  </svg>
-                </x-secondary-button>
-              </a>
+                        @if($comments->count() > 0)
+                            @foreach($comments as $comment)
+                                <div class="flex flex-col mb-4">
+                                    
+                                    <p class="py-2 px-3 text-gray-800 dark:text-gray-200" id="comment_content">  
+                                        {{ $comment->content }}
+                                    </p>
+                                </div>
+                            @endforeach
+                        @else
+                            <p>No comments available.</p>
+                        @endif
+                        <div class="flex items-center justify-end mt-4">
+                          <a href="{{ url()->previous() }}">
+                            <x-secondary-button class="ml-3">
+                              {{ __('Back') }}
+                            </x-secondary-button>
+                          </a>
+                          <a href="{{ route('tweet.send',$tweet->id) }}">
+                            <x-secondary-button class="ml-3">
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z" />
+                              </svg>
+                            </x-secondary-button>
+                          </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </x-app-layout>
